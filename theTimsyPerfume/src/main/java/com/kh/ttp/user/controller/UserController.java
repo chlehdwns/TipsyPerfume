57dd5283b2250e692eddd5a66bc99a32a5496eb8
ji-min-2020 <65524574+ji-min-286011@users.noreply.github.com>
2023-11-14 16:30:32 +0900

    킼킼 다만들었땅

--- theTimsyPerfume/src/main/java/com/kh/ttp/user/controller/UserController.java
+++ theTimsyPerfume/src/main/java/com/kh/ttp/user/controller/UserController.java
@@ -1,9 +1,15 @@
 package com.kh.ttp.user.controller;
 
+import javax.servlet.http.HttpSession;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.ModelAndView;
 
 import com.kh.ttp.user.model.service.UserService;
+import com.kh.ttp.user.model.vo.User;
 
 @Controller
 public class UserController {
@@ -12,6 +18,37 @@ public class UserController {
 	private UserService userService;
 	
 	
+ 
+	@RequestMapping("loginForm.me")
+	public String loginUser() {
+		return "member/LoginForm";
+	}
+	
+	
+	
+	@RequestMapping("login.me")
+	public ModelAndView loginUser(User u,
+									HttpSession session,
+									ModelAndView mv) {
+		
+		System.out.println("userEmail : " + u.getUserEmail());
+		System.out.println("userPwd : " + u.getUserPwd());
+		
+		User loginUser = userService.loginUser(u);
+		
+		
+		if(loginUser != null) {
+			session.setAttribute("loginUser", loginUser);
+			mv.setViewName("redirect:/");
+		} else {
+			// model.addAttribute
+			mv.addObject("alertMsg", "로그인 실패ㅠ");
+			mv.setViewName("common/errorPage");
+		}
+		
+		return mv;
+	}
+
 	
 	
 	
