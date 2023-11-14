bd94d19038c7060c7a82e80c30e0e1018c8e62b4
JYChloeMok <137197469+JYChloeMok@users.noreply.github.com>
2023-11-14 12:39:22 +0900

    edit Receiver vo - MJY

---
+++ theTimsyPerfume/src/main/java/com/kh/ttp/order/model/vo/Receiver.java
@@ -0,0 +1,19 @@
+package com.kh.ttp.order.model.vo;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+
+@Data @AllArgsConstructor
+public class Receiver {
+
+	private int receiverNo;
+	private int userNo;
+	private String receiverName;
+	private String phone;
+	private String address;
+	private String addressDetail;
+	private int postalCode;
+	private String placeAlias;
+	private String primaryStatus;
+	
+}
