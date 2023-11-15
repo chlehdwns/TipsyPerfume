b0d85152e46a22badbe222f0872e12b9c004c5c0
LeeMj521 <129156800+LeeMj521@users.noreply.github.com>
2023-11-15 14:58:25 +0900

    re

--- theTimsyPerfume/src/main/java/com/kh/ttp/community/controller/ReviewController.java
+++ theTimsyPerfume/src/main/java/com/kh/ttp/community/controller/ReviewController.java
@@ -6,11 +6,14 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.servlet.ModelAndView;
 
+import com.google.gson.Gson;
 import com.kh.ttp.common.model.vo.PageInfo;
 import com.kh.ttp.common.template.Pagination;
 import com.kh.ttp.community.model.service.ReviewService;
+import com.kh.ttp.community.model.vo.NoticeVO;
 import com.kh.ttp.community.model.vo.ReviewVO;
 
 @Controller
@@ -40,5 +43,14 @@ public class ReviewController {
 		setViewName("community/reviewDetail");
 		
 		return mv;
+	}
+	@GetMapping(value = "reviewDetailList", produces = "application/json; charset=UTF-8")
+	@ResponseBody
+	public String reviewDetailList(int page) {
+		int listCount = reviewService.countReviewList();
+		PageInfo pi = Pagination.getPageInfo(listCount, page, 9, 5);
+		ArrayList<ReviewVO> list = reviewService.seletcReviewList(pi);
+		
+		return new Gson().toJson(list);
 	}
 }
