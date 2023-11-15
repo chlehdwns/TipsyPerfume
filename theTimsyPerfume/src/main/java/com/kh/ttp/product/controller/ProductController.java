bb8e09f2eda09e0d10059f2db4501295a421949e
JYChloeMok <137197469+JYChloeMok@users.noreply.github.com>
2023-11-15 15:43:14 +0900

    css조정 - mjy

--- theTimsyPerfume/src/main/java/com/kh/ttp/product/controller/ProductController.java
+++ theTimsyPerfume/src/main/java/com/kh/ttp/product/controller/ProductController.java
@@ -1,27 +1,38 @@
 package com.kh.ttp.product.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.servlet.ModelAndView;
+
+import com.kh.ttp.product.model.service.ProductService;
 
 @Controller
 public class ProductController {
 	
+	@Autowired
+	private ProductService productService;
 
 	/**
 	 * 상품 메인페이지 조회 기능
 	 * @return
 	 */
 	@GetMapping("productMain.pr")
-	public String productMain(@RequestParam (value="pdtIdenKey", defaultValue="M")String pdtIdenKey) { // Q.알아서 관리하니까 불변객체 자원소모는 신경 안써도 되나?
+	public String productMain(@RequestParam (value="pdtIdenKey", defaultValue="M")String pdtIdenKey,
+											 int pdtNo) { // Q.알아서 관리하니까 불변객체 자원소모는 신경 안써도 되나?
 		
 		if("A".equals(pdtIdenKey.toUpperCase())) {
 			// 알콜 조회 A
 			System.out.println("알콜조회");
+			
+			
+			
 		} else if("P".equals(pdtIdenKey.toUpperCase())) {
 			// 향수 조회 P
 			System.out.println("향수조회");
+			
+			
+			
 		} else {
 			System.out.println("기타등등");
 			// common/errorPage  ${ errorMsg }
