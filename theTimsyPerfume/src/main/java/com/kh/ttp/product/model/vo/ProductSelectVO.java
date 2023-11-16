817d82b0c752848600f82d8afd7764e9765b25b7
JYChloeMok <137197469+JYChloeMok@users.noreply.github.com>
2023-11-17 01:20:59 +0900

    create product main - MJY

--- theTimsyPerfume/src/main/java/com/kh/ttp/product/model/vo/ProductSelectVO.java
+++ theTimsyPerfume/src/main/java/com/kh/ttp/product/model/vo/ProductSelectVO.java
@@ -1,5 +1,27 @@
 package com.kh.ttp.product.model.vo;
 
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data @NoArgsConstructor
 public class ProductSelectVO {
+	
+	/*
+	PDT_NO,
+	PDT_NAME,
+	PDT_INTRO,
+	PDT_DESCRIPTION,
+	PDT_MANUFAC,
+	REVIEW_AVG,
+	PDT_IMG_SRC
+	*/
+	
+	private int pdtNo;
+	private float reviewAvg;
+	private String pdtName;
+	private String pdtIntro;
+	private String pdtDescription;
+	private String pdtManufac;
+	private String pdtImgSrc;
 
 }
